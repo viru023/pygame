37b7b7e6502a2ebf010c4b17d03b666d63050e5e
viru <46395306+viru023@users.noreply.github.com>
2021-10-21 07:16:34 +0530

    added lives

--- randomnumgame.py
+++ randomnumgame.py
@@ -1,9 +1,13 @@
 import random
-num1 = random.randint(1,9)
+
+num1 = random.randint(1,10)
 as_string = str(num1)
-num2 = int(input("enter a number: "))
-if num1 == num2:
-    print("correct")
-else:
-    print("random number is " +as_string)
-    print("try again!!!")
+for i in range(3):
+    num2 = int(input("enter a number: "))
+    if num1 == num2:
+        print("correct")
+    else:
+        print("random number is " +as_string)
+        print("try again!!!")
+    i-=1
+print("no more lives")
